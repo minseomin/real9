4dfc01cf55a5d24929e3e24321a250a97900924f
minseomin <113421574+minseomin@users.noreply.github.com>
2022-11-01 16:53:56 +0900

    03-2

--- main.c
+++ main.c
@@ -6,20 +6,18 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i, average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	sum=0;
-	for (i=0;i<SIZE;i++)
-{	
-	printf("학생 성적을 입력하세요: \n");
-	scanf("%d", &grade[i]);
-	sum+=grade[i];
-}
+	for(i=0;i<SIZE;i++)
+		grade[i]=rand() %100;
 		
-	average = sum /SIZE;
-	printf("성적 평균 : %d \n", average);
-	
+	for(i=0;i<SIZE;i++)
+		score[i]=grade[i];
+		
+	for(i=0;i<SIZE;i++)
+		printf("score[%d]=%d\n,i,score[i]");
+		
 	return 0;
 }
